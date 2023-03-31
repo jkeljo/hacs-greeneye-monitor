3a165099772839f8a09aee88200302a41c54d815
Jonathan Keljo <jkeljo@users.noreply.github.com>
2023-03-31 19:51:32 +0000

    Trim unused constants

--- custom_components/greeneye_monitor/const.py
+++ custom_components/greeneye_monitor/const.py
@@ -1,10 +1,9 @@
-"""Shared constants for the greeneye_monitor integration."""
+"""Constants for the greeneye_monitor component."""
 
 CONF_CHANNELS = "channels"
 CONF_COUNTED_QUANTITY = "counted_quantity"
 CONF_COUNTED_QUANTITY_PER_PULSE = "counted_quantity_per_pulse"
 CONF_DEVICE_CLASS = "device_class"
-CONF_MONITOR_SERIAL_NUMBER = "monitor"
 CONF_MONITORS = "monitors"
 CONF_NET_METERING = "net_metering"
 CONF_NUMBER = "number"
@@ -16,10 +15,5 @@ CONF_VOLTAGE_SENSORS = "voltage"
 
 DATA_GREENEYE_MONITOR = "greeneye_monitor"
 DOMAIN = "greeneye_monitor"
-
-SENSOR_TYPE_CURRENT = "current_sensor"
-SENSOR_TYPE_PULSE_COUNTER = "pulse_counter"
-SENSOR_TYPE_TEMPERATURE = "temperature_sensor"
-SENSOR_TYPE_VOLTAGE = "voltage_sensor"
 
 TEMPERATURE_UNIT_CELSIUS = "C"
